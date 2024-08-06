8c51b276240fa7dc0983b00c43a264996dc1ceba
Ankit Kumar Upadhyay <ankitupadhyay4519@gmail.com>
2024-08-06 09:58:08 +0530

    Live Api added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './body.css'
 import RestaurantCard from './RestaurantCard'
-import API_Data from '../utils/api'
 
 const Body = () => {
+    const[listOfRestaurants,setListOfRestaurants] =useState([]);
 
-    const filterCards=()=>{
-        console.log("Filtered");
+    const filterCardsRated=()=>{
+        setListOfRestaurants(listOfRestaurants.filter((res)=>(
+            res.info.avgRating > 4.3
+        )))
+       
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[]);
+
+    const fetchData= async ()=>{
+        const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+        const json = await data.json();
+        
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
     }
 
     return (
         <div className="body">
             <div className="search">
-                <button className="filterBtn" onClick={filterCards} >Top rated Restaurants</button>
+                <button className="filterBtn" onClick={()=> filterCardsRated()} >Top rated Restaurants</button>
+                
             </div>
             <div className="resContainer">
                 {
-                    API_Data.map((restaurant)=>(
+                    listOfRestaurants.map((restaurant)=>(
                         <RestaurantCard key={restaurant.info.id} resData={restaurant}/>
                     ))
                 }
